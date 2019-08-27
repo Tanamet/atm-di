82ea71cc1e004c26e62c44f6e06d34149cd6a7fc
tanamet.t <tanamet.tpk@gmail.com>
2019-08-27 19:31:30 +0700

    + bean in atm config

--- src/main/java/th/ku/Main.java
+++ src/main/java/th/ku/Main.java
@@ -1,13 +1,21 @@
 package th.ku;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        DataSource dataSource = new DataSource("customers.txt");
-        Bank bank = new Bank(dataSource);
-        ATM atm = new ATM(bank);
-        ATMSimulator atmSimulator = new ATMSimulator(atm);
+//        DataSource dataSource = new DataSource("customers.txt");
+//        Bank bank = new Bank(dataSource);
+//        ATM atm = new ATM(bank);
+//        ATMSimulator atmSimulator = new ATMSimulator(atm);
+//        atmSimulator.run();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("atm-config.xml");
+        ATMSimulator atmSimulator = context.getBean("atmSimulator", ATMSimulator.class);
+
         atmSimulator.run();
 
     }

de200824d1ac41ae823e87b5151376cf69dd139a
tanamet.t <tanamet.tpk@gmail.com>
2019-08-27 19:20:24 +0700

    + atm config

--- src/main/java/th/ku/Main.java
+++ src/main/java/th/ku/Main.java
@@ -1,11 +1,15 @@
 package th.ku;
 
 public class Main {
+
     public static void main(String[] args) {
+
         DataSource dataSource = new DataSource("customers.txt");
         Bank bank = new Bank(dataSource);
         ATM atm = new ATM(bank);
         ATMSimulator atmSimulator = new ATMSimulator(atm);
         atmSimulator.run();
+
     }
+
 }

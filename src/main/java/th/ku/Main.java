6fe259e7336700c80e869d76b511611cb035dc62
tanamet.t <tanamet.tpk@gmail.com>
2019-08-27 19:11:42 +0700

    + atm from ladyusa

---
+++ src/main/java/th/ku/Main.java
@@ -0,0 +1,11 @@
+package th.ku;
+
+public class Main {
+    public static void main(String[] args) {
+        DataSource dataSource = new DataSource("customers.txt");
+        Bank bank = new Bank(dataSource);
+        ATM atm = new ATM(bank);
+        ATMSimulator atmSimulator = new ATMSimulator(atm);
+        atmSimulator.run();
+    }
+}
